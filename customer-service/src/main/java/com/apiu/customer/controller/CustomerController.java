0a1a299de3ca6dc9423813141bf11e02f1eb6586
Juan Quinto Personal <juans.quintoh@hotmail.com>
2026-05-15 21:50:57 -0500

    fix(customer): add missing List import in CustomerController

--- customer-service/src/main/java/com/apiu/customer/controller/CustomerController.java
+++ customer-service/src/main/java/com/apiu/customer/controller/CustomerController.java
@@ -10,6 +10,8 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
+import java.util.List;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
